9cddabff9e74e742412e7280ffc496127f275e4d
Daniel Vollmer <daniel.vollmer98@gmail.com>
2019-05-06 00:15:47 +0200

    Changed the names of the test methods in GeneratorTest.

--- iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
+++ iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
@@ -3,12 +3,20 @@ package org.jis.generator;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import javax.imageio.ImageIO;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 
 /**
  * Test class for generator
@@ -20,88 +28,130 @@ public class GeneratorTest {
 
 	private Generator generator;
 	private BufferedImage image;
+	private BufferedImage rotatedImage;
 
-	/**
-	 * 
-	 * @throws Exception
-	 */
+	@BeforeClass
+	public static void setUpClass() {
+		File dir = new File("target/test");
+		dir.mkdir();
+	}
+
 	@Before
 	public void setUp() throws Exception {
 		generator = new Generator(null, 0);
-		BufferedImage input = ImageIO.read(this.getClass().getResourceAsStream("/image.jpg"));
-		image = new BufferedImage(input.getWidth(), input.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		image = ImageIO.read(this.getClass().getResourceAsStream("/image.jpg"));
 	}
 
-	/**
-	 * 
-	 * @throws Exception
-	 */
 	@After
 	public void tearDown() throws Exception {
+		java.util.Date time = Calendar.getInstance().getTime();
+		SimpleDateFormat formatter = new SimpleDateFormat("MM-dd_HH.mm.ss.SSS");
+		String date = formatter.format(time);
+		if (rotatedImage != null) {
+			String imageName = testMethodName.getMethodName() + "_" + date;
+			File outputfile = new File("target/test/" + imageName + ".jpg");
+			ImageIO.write(rotatedImage, "jpg", outputfile);
+		}
+	}
+
+	@Rule
+	public TestName testMethodName = new TestName();
+
+	@Test
+	public void noRotationTest() {
+		rotatedImage = generator.rotateImage(image, 0.0);
+		assertEquals(rotatedImage, image);
 	}
 
 	@Test
-	public void testRotateImageNoRotation() {
-		BufferedImage newImage = generator.rotateImage(image, 0.0);
-		assertEquals(newImage, image);
-	}
-
-	@Test
-	public void testRotateImageNullImage() {
+	public void nullImageTest() {
 		assertNull(generator.rotateImage(null, 0.0));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testRotateImageNotMultipleOf90() {
+	public void rotateImageNotMultipleOf90Test() {
 		generator.rotateImage(image, 0.42);
 	}
 
 	@Test
-	public void testRotateImageBy90Degree() {
-		BufferedImage rotatedImage = generator.rotateImage(image, Math.toRadians(90));
+	public void rotateImageBy90DegreeTest() {
+		rotatedImage = generator.rotateImage(image, Math.toRadians(90));
+
 		assertEquals(image.getHeight(), rotatedImage.getWidth());
 		assertEquals(image.getWidth(), rotatedImage.getHeight());
-		// assertEquals(image, rotatedImage);
+
+		boolean sameImage = true;
+
+		int width = image.getWidth();
+		int height = image.getHeight();
+
+		try {
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
+					if (image.getRGB(x, y) != rotatedImage.getRGB(height - 1 - y, x)) {
+						sameImage = false;
+						break;
+					}
+				}
+			}
+		} catch (IndexOutOfBoundsException e) {
+			fail("Error, height or width of rotated image is not correct!");
+		}
+		assertTrue(sameImage);
 	}
 
 	@Test
-	public void testRotateImageBy270Degree() {
-		BufferedImage rotatedImage = generator.rotateImage(image, Math.toRadians(270));
+	public void rotateImageBy270DegreeTest() {
+		rotatedImage = generator.rotateImage(image, Math.toRadians(270));
 		assertEquals(image.getHeight(), rotatedImage.getWidth());
 		assertEquals(image.getWidth(), rotatedImage.getHeight());
-		// assertEquals(image, rotatedImage);
+
+		boolean sameImage = true;
+
+		int width = image.getWidth();
+		int height = image.getHeight();
+		try {
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
+					if (image.getRGB(x, y) != rotatedImage.getRGB(y, width - 1 - x)) {
+						sameImage = false;
+						break;
+					}
+				}
+			}
+		} catch (IndexOutOfBoundsException e) {
+			fail("Error, height or width of rotated image is not correct!");
+		}
+		assertTrue(sameImage);
 	}
 
 	@Test
-	public void testRotateImageByMinus90Degree() {
-		BufferedImage rotatedImage = generator.rotateImage(image, Math.toRadians(-90));
+	public void rotateImageByMinus90DegreeTest() {
+		rotatedImage = generator.rotateImage(image, Math.toRadians(-90));
 		assertEquals(image.getHeight(), rotatedImage.getWidth());
 		assertEquals(image.getWidth(), rotatedImage.getHeight());
 		assertTrue(compareImages(rotatedImage, generator.rotateImage(image, Math.toRadians(270))));
 	}
 
 	@Test
-	public void testRotateImageByMinus270Degree() {
-		BufferedImage rotatedImage = generator.rotateImage(image, Math.toRadians(-270));
+	public void rotateImageByMinus270DegreeTest() {
+		rotatedImage = generator.rotateImage(image, Math.toRadians(-270));
 		assertEquals(image.getHeight(), rotatedImage.getWidth());
 		assertEquals(image.getWidth(), rotatedImage.getHeight());
 		assertTrue(compareImages(rotatedImage, generator.rotateImage(image, Math.toRadians(90))));
 	}
 
-	public static boolean compareImages(BufferedImage imgA, BufferedImage imgB) {
-		// The images must be the same size.
-		if (imgA.getWidth() != imgB.getWidth() || imgA.getHeight() != imgB.getHeight()) {
+	public boolean compareImages(BufferedImage imageA, BufferedImage imageB) {
+		if (imageA.getWidth() != imageB.getWidth() || imageA.getHeight() != imageB.getHeight()) {
 			return false;
 		}
 
-		int width = imgA.getWidth();
-		int height = imgA.getHeight();
+		int width = imageA.getWidth();
+		int height = imageA.getHeight();
 
-		// Loop over every pixel.
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
-				// Compare the pixels for equality.
-				if (imgA.getRGB(x, y) != imgB.getRGB(x, y)) {
+				if (imageA.getRGB(x, y) != imageB.getRGB(x, y)) {
 					return false;
 				}
 			}
